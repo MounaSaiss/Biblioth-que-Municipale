eede0c656cbf2ff4eee888d249bd680bac64206e
LunaDevX <mouna1.saiss@gmail.com>
2026-02-02 07:42:53 +0100

    Add debug statement for request capture and update book routes for consistency

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [BookController::class, 'index'])->name('books.index');
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
-Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
-Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
-Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+Route::get('/books/show', [BookController::class, 'show'])->name('books.show');
+Route::get('/books/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/update', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/destroy', [BookController::class, 'destroy'])->name('books.destroy');
