e4b5bb552c5d7065f8900497beaa0b6c59a2e1d5
LunaDevX <mouna1.saiss@gmail.com>
2026-02-02 07:36:58 +0100

    Refactor routes to set the homepage to the books index view

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
 Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
