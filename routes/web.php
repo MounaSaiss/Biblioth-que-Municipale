470185e16721555917c520d10499aa8c56b3c90d
LunaDevX <mouna1.saiss@gmail.com>
2026-02-02 07:37:24 +0100

    Fix route definition for books index to ensure proper functionality

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/', [BookController::class, 'index'])->name('books.index');
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books/store', [BookController::class, 'store'])->name('books.store');
 Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
